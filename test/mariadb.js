1594f4e6c5f7c205ae4748231090cfd7d712092a
Luke Childs <lukechilds123@gmail.com>
2017-08-15 15:08:10 +0100

    Better DB user names

--- test/mariadb.js
+++ test/mariadb.js
@@ -3,7 +3,7 @@ import keyvTestSuite, { keyvOfficialTests } from '@keyv/test-suite';
 import Keyv from 'keyv';
 import KeyvMysql from 'this';
 
-keyvOfficialTests(test, Keyv, 'mysql://root@localhost:3330/test', 'mysql://foo');
+keyvOfficialTests(test, Keyv, 'mysql://mariadb@localhost:3330/test', 'mysql://foo');
 
-const store = () => new KeyvMysql('mysql://root@localhost:3330/test');
+const store = () => new KeyvMysql('mysql://mariadb@localhost:3330/test');
 keyvTestSuite(test, Keyv, store);
